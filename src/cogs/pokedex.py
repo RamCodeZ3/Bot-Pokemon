5beec8ed7c8f159869c04b3dd3ffe10760a0ac35
RamCodeZ3 <arammusset7@gmail.com>
2025-09-02 16:02:11 -0400

    Se mejoro la esctructura del codigo siguiengo el PEP8

--- src/cogs/pokedex.py
+++ src/cogs/pokedex.py
@@ -5,6 +5,7 @@ import requests
 import json
 import os
 
+# Rutas
 PATH_JSON = os.path.abspath("data")
 URL = "https://pokeapi.co/api/v2/pokemon/"
 SPECIES_URL = "https://pokeapi.co/api/v2/pokemon-species/"
@@ -47,24 +48,36 @@ class Pokedex(commands.Cog):
             # Buscar descripción en español, si no existe usar inglés
             for entry in species_data["flavor_text_entries"]:
                 if entry["language"]["name"] == "es":
-                    descripcion = entry["flavor_text"].replace("\n", " ").replace("\f", " ")
+                    descripcion = entry["flavor_text"].replace(
+                        "\n",
+                        " "
+                    ).replace("\f", " ")
                     break
             else:  # Si no hay en español, usa inglés
                 for entry in species_data["flavor_text_entries"]:
                     if entry["language"]["name"] == "en":
-                        descripcion = entry["flavor_text"].replace("\n", " ").replace("\f", " ")
+                        descripcion = entry["flavor_text"].replace(
+                            "\n",
+                            " "
+                        ).replace("\f", " ")
                         break
             color_embed = colores_pokemon.get(color.lower(), 0xFF0000)
-            gen_data = generations.get(region, {"name": "Desconocida", "region": "Desconocida"})
+            gen_data = generations.get(region, {
+                "name": "Desconocida",
+                "region": "Desconocida"})
 
             # Datos básicos
             name_poke = data["name"].capitalize()
             poke_id = data["id"]
             types = [t["type"]["name"] for t in data["types"]]
-            types_str = "\n".join([f"{types_emojis.get(t, '')} {t.capitalize()}" for t in types])
+            types_str = "\n".join([
+                f"{types_emojis.get(t, '')} {t.capitalize()}" for t in types
+                ])
             weight = data["weight"]
             height = data["height"]
             imagen = data["sprites"]["front_default"]
+            generation = f"\nGeneracion: **{gen_data["name"].capitalize()}**"
+            region_poke = f"\nRegion: **{gen_data["region"].capitalize()}**"
 
             # weaknesses
             weaknesses = set()
@@ -72,26 +85,47 @@ class Pokedex(commands.Cog):
                 type_response = requests.get(TYPE_URL + tipo)
                 if type_response.status_code == 200:
                     type_data = type_response.json()
-                    for weakness in type_data["damage_relations"]["double_damage_from"]:
+                    type = type_data["damage_relations"]["double_damage_from"]
+                    for weakness in type:
                         weaknesses.add(weakness["name"])
-            weaknesses_str = "-".join([f"{types_emojis.get(w, '')} {w.capitalize()}" for w in weaknesses]) if weaknesses else "Ninguna"
+            weaknesses_str = "-".join(
+                [
+                    f"{types_emojis.get(w, '')} {w.capitalize()}" 
+                    for w in weaknesses
+                ]) if weaknesses else "Ninguna"
 
             # Embed de Discord
             embed = discord.Embed(
                 title="{} (#{:04})".format(name_poke, poke_id),
-                description=f"{descripcion}\n\nGeneracion: **{gen_data["name"].capitalize()}**\nRegion: **{gen_data["region"].capitalize()}**",
+                description=f"{descripcion}\n{generation}{region_poke}",
                 color=int(color_embed, 16)
             )
 
-            embed.add_field(name="Tipos", value=types_str, inline=True)
-            embed.add_field(name="Altura", value=f"{height}m", inline=True)
-            embed.add_field(name="Peso", value=f"{weight}kg", inline=True)
-            embed.add_field(name="Debilidades", value=weaknesses_str, inline=False)
+            embed.add_field(
+                name="Tipos",
+                value=types_str,
+                inline=True
+            )
+            embed.add_field(
+                name="Altura",
+                value=f"{height}m",
+                inline=True
+            )
+            embed.add_field(
+                name="Peso",
+                value=f"{weight}kg",
+                inline=True
+                )
+            embed.add_field(
+                name="Debilidades",
+                value=weaknesses_str,
+                inline=False
+            )  
             embed.set_thumbnail(url=imagen)
             embed.set_footer(text="Datos obtenidos de la PokeAPI")
 
             await interaction.channel.send(embed=embed)
-        
+
         else: 
             await interaction.channel.send("No se encontro el pokemon")
 
