0c86e4f3bb2c8ca84a4afe02e8e22950bf866f37
RamCodeZ3 <arammusset7@gmail.com>
2025-10-08 16:56:02 -0400

    Se arreglo el error de que no se enviaba el embed y se agrego un nuevo comando

--- src/cogs/game.py
+++ src/cogs/game.py
@@ -12,14 +12,14 @@ class Game(commands.Cog):
 
     @app_commands.command(
         name="play",
-        description="comando para comenzar un juego de adivinaza con el bot"
+        description="Comando para comenzar un juego de adivinanza con el bot"
     )
     async def play(self, interaction: discord.Interaction):
-        response = requests.get(URL + random.randint(1, 1000))
+        response = requests.get(URL + str(random.randint(1, 1000)))
 
         if response.status_code == 200:
             data = response.json()
-            # Peso y altura (directo)
+            correct_name = data["name"]
             weight = data["weight"]
             height = data["height"]
             types = [t["type"]["name"] for t in data["types"]]
@@ -27,46 +27,56 @@ class Game(commands.Cog):
             games = [g["version"]["name"] for g in data["game_indices"]]
             species_url = data["species"]["url"]
             species_data = requests.get(species_url).json()
+            imagen = data["sprites"]["front_default"]
             generation = species_data["generation"]["name"]
 
-            # Ahora pedimos la generación y obtenemos la región
             gen_url = species_data["generation"]["url"]
             gen_data = requests.get(gen_url).json()
             region = gen_data["main_region"]["name"]
 
-            # Armamos el array con todo
-            pokemon_info = [{
+            pokemon_info = {
                 "weight": f"El pokemon pesa {weight}kg",
                 "height": f"El pokemon mide {height}m de alto",
-                "tipes": f"Es tipo: {types}",
-                "games": f"Aparece en estos juegos: {weight}",
-                "region": f"Su region es {region}",
-                "ability": f"Tiene estas habilidades{weight}"
-            }]
+                "types": f"Es tipo: {', '.join(types)}",
+                "games": f"Aparece en estos juegos: {', '.join(games[:5])}",
+                "region": f"Su región es {region}",
+                "ability": f"Tiene estas habilidades: {', '.join(ability)}"
+            }
 
-        embed = discord.Embed(
-            title="Adivina el pokemon",
-            description="TieneS 3 intentos para adivinar el pokemon",
-            color=(0x800080)
-        )
-        embed.add_field(
-                name="Pista1:",
-                value=pokemon_info["weight"],
-                inline=False
-        )
-        embed.add_field(
-                name="Pista2:",
-                value=pokemon_info["height"],
-                inline=False
-        )
-        embed.add_field(
-                name="Pista3:",
-                value=pokemon_info["types"],
-                inline=False
-        )
-        embed.set_footer(text="Datos obtenidos de la PokeAPI")
-        
-        await interaction.channel.send(embed=embed)
+            embed = discord.Embed(
+                title="Adivina el Pokémon",
+                description="Tienes 3 intentos para adivinar el Pokémon",
+                color=0x800080
+            )
+            embed.add_field(name="pokemon:", value=f"{correct_name}", inline=False)
+            embed.add_field(name="Pista 1:", value=pokemon_info["weight"], inline=False)
+            embed.add_field(name="Pista 2:", value=pokemon_info["height"], inline=False)
+            embed.add_field(name="Pista 3:", value=pokemon_info["types"], inline=False)
+            embed.set_footer(text="Datos obtenidos de la PokeAPI")
+
+            await interaction.response.send_message(embed=embed)
+            return [correct_name, imagen]
+        else:
+            await interaction.response.send_message("No se pudo obtener información del Pokémon.")
+    
+    @app_commands.command(
+        name='answer',
+        description='Responde con el nombre del pokemon'
+    )
+    async def answer(self, interaction: discord.Interaction, *, name: str):
+        data = Game.play() 
+        if name == data[0]:
+            embed = discord.Embed(
+                title="Correcto el pokemon era: {}".format(name),
+                description="Ere todo un maestro pokemon",
+                color=0x800080
+            )
+            embed.set_image(data[1])
+            embed.set_footer(text="Datos obtenidos de la PokeAPI")
+            await interaction.response.send_message(embed=embed)
+        else:
+            await interaction.response.send_message("Lo siento pero no es l pokemon correcto.")
+
 
 async def setup(bot):
     await bot.add_cog(Game(bot))
